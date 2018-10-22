44d9a339678deb5c54784a0c0d30658cd2bbe74e
srfoster <stephen@thoughtstem.com>
2018-10-22 13:52:29 -0700

    enforcing hy install on linux

--- circuit-playground-module.rkt
+++ circuit-playground-module.rkt
@@ -34,13 +34,17 @@
   (require "./api/accel.rkt")
   (require "./api/servo.rkt")
   (require "./api/ir.rkt")
-  (require "./api/rgb_helper.rkt"))
+  (require "./api/rgb_helper.rkt")
+ 
 
-(define v (with-output-to-string 
-            (thunk (system "hy --version"))))
+  (and (eq? (system-type 'os) 'unix)
+       (not 
+        (string-contains? 
+         (with-output-to-string 
+           (thunk (system "hy --version")))
+         "0.14"))
+       (begin
+         (displayln "One moment.  Installing hy.  This is a one-time thing.")
+         (system "sudo pip install git+http://github.com/hylang/hy.git@0.14.0")))
 
-(and (not (string-contains? v "0.14"))
-     (begin
-        (displayln "One moment.  Installing hy.  This is a one-time thing.")
-        (system "sudo pip install git+http://github.com/hylang/hy.git@0.14.0")))
-
+  )
